9de8f27bbfd204acecf1807ead0ed7eb314ea432
Amit Thatera <amitthatera.dev@gmail.com>
2024-06-02 22:49:40 +0530

    add repository

---
+++ src/main/java/com/store/grocery/fresh_express/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.store.grocery.fresh_express.repository;
+
+import com.store.grocery.fresh_express.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long>{
+
+}
---
+++ src/main/java/com/store/grocery/fresh_express/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.store.grocery.fresh_express.repository;
+
+import com.store.grocery.fresh_express.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
